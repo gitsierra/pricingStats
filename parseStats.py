7ca5dfa6a46173843bef03950369c2253280a05e
System Administrator <root@Samuels-MacBook-Pro.local>
2019-05-30 23:06:05 -0400

    parses file and puts each stock object into an array

--- parseStats.py
+++ parseStats.py
@@ -20,4 +20,27 @@ info_array=results.file.readlines()
 
 print 'timestamp1   =', results.timestamp1
 print 'timestamp2 =', results.timestamp2
-print info_array
+
+class Stock:
+    def __init__(self,timestamp,price,amount):
+        self.timestamp=timestamp #need to parse timestamp
+        self.price=price
+        self.amount=amount
+    def __str__(self):
+        return str(self.__class__)+": "+str(self.__dict__)
+
+def make_stock(data_string):
+    data_array=data_string.split()
+    stock=Stock(data_array[0],float(data_array[1]),int(data_array[2]))
+    return stock.__dict__
+
+all_stocks=[]
+for stats in info_array:
+    stock=make_stock(stats)
+    print stock
+    all_stocks.append(stock)
+
+print all_stocks
+
+
+
